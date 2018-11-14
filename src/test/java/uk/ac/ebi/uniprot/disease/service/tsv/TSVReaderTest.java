9d4b5f0f99ed9dba526003d1e4fab5d3677eced4
Shadab Ahmad <sahmad@dloan110-ml.windows.ebi.ac.uk>
2018-11-14 14:22:16 +0000

    TRM-20995 download and parse disgenet data. try with resources use

--- src/test/java/uk/ac/ebi/uniprot/disease/service/tsv/TSVReaderTest.java
+++ src/test/java/uk/ac/ebi/uniprot/disease/service/tsv/TSVReaderTest.java
@@ -14,17 +14,15 @@ public class TSVReaderTest {
 
     @Test
     public void testGetRecords() throws IOException {
-        TSVReader tsvReader = new TSVReader("src/test/resources/sample_gene_disease_association.tsv");
-        int recordCount = 0;
-        while(tsvReader.hasMoreRecord()){
-            List<String> record = tsvReader.getRecord();
-            verifyRecord(record);
-            recordCount++;
+        try(TSVReader tsvReader = new TSVReader("src/test/resources/sample_gene_disease_association.tsv")) {
+            int recordCount = 0;
+            while (tsvReader.hasMoreRecord()) {
+                List<String> record = tsvReader.getRecord();
+                verifyRecord(record);
+                recordCount++;
+            }
+            Assert.assertEquals("Record count is not equal", 20, recordCount);
         }
-
-        Assert.assertEquals("Record count is not equal", 20, recordCount);
-
-        tsvReader.close();
     }
 
     private void verifyRecord(List<String> record){
