4de42c52fc958e371e640b40bc5f919a865d603d
Shadab Ahmad <sahmad@dloan110-ml.windows.ebi.ac.uk>
2018-11-20 16:05:23 +0000

    Remvoe unnecessary equals and hashcode

--- src/main/java/uk/ac/ebi/uniprot/disease/pipeline/request/DiseaseRequest.java
+++ src/main/java/uk/ac/ebi/uniprot/disease/pipeline/request/DiseaseRequest.java
@@ -12,7 +12,6 @@ import java.util.List;
  */
 @Getter
 @Setter
-@EqualsAndHashCode
 @ToString
 @Builder
 public class DiseaseRequest {
--- src/main/java/uk/ac/ebi/uniprot/disease/pipeline/request/WorkflowMetrics.java
+++ src/main/java/uk/ac/ebi/uniprot/disease/pipeline/request/WorkflowMetrics.java
@@ -11,7 +11,6 @@ import lombok.ToString;
  */
 @Getter
 @Setter
-@EqualsAndHashCode
 @ToString
 public class WorkflowMetrics {
     private long startTime;
