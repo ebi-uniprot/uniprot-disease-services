b5bc4874323b122fc6ff713be15587049be32c38
Shadab Ahmad <sahmad@ebi.ac.uk>
2019-01-31 13:17:41 +0000

    add a rest service module

---
+++ rest-service/src/main/java/uk/ac/ebi/uniprot/rest/DiseaseRestServiceApplication.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.uniprot.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiseaseRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiseaseRestServiceApplication.class, args);
+    }
+
+}
+
