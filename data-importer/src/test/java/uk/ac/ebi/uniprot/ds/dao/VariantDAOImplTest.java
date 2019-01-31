c2f06eb0fd03338f0549a3e87a88c9c0f93e72bc
Shadab Ahmad <sahmad@ebi.ac.uk>
2019-01-31 11:32:23 +0000

    TRM-21482 fix jenkins job

--- data-importer/src/test/java/uk/ac/ebi/uniprot/ds/dao/VariantDAOImplTest.java
+++ data-importer/src/test/java/uk/ac/ebi/uniprot/ds/dao/VariantDAOImplTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.ac.ebi.uniprot.ds.model.*;
