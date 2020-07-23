945e879633717bbac48190469495747b3f97b4ab
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-07-23 11:54:10 +0100

    add cors mapping

--- disease-graphql-service/src/main/java/uk/ac/ebi/uniprot/ds/graphql/DiseaseGraphQLApplication.java
+++ disease-graphql-service/src/main/java/uk/ac/ebi/uniprot/ds/graphql/DiseaseGraphQLApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import uk.ac.ebi.uniprot.ds.graphql.model.mapper.DiseaseToDiseaseType;
 import uk.ac.ebi.uniprot.ds.graphql.model.mapper.ProteinToProteinType;
@@ -35,5 +37,17 @@ public class DiseaseGraphQLApplication {
 		modelMapper.addMappings(new DiseaseToDiseaseType());
 		modelMapper.addMappings(new ProteinToProteinType());
 		return modelMapper;
+	}
+
+	static final String ALLOW_ALL_ORIGINS = "*";
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/graphql/**").allowedOrigins(ALLOW_ALL_ORIGINS);
+				registry.addMapping("/graphql/*").allowedOrigins(ALLOW_ALL_ORIGINS);
+			}
+		};
 	}
 }
