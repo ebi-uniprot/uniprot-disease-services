fb75247180b15a91757f3201d031e4ede6369800
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-09-07 15:14:49 +0100

    add dependency bean in test

--- disease-rest-service/src/test/java/uk/ac/ebi/uniprot/ds/rest/controller/ProteinControllerGetDiseasesTest.java
+++ disease-rest-service/src/test/java/uk/ac/ebi/uniprot/ds/rest/controller/ProteinControllerGetDiseasesTest.java
@@ -36,6 +36,7 @@ import uk.ac.ebi.uniprot.ds.rest.RestServiceSpringBootApplication;
 import uk.ac.ebi.uniprot.ds.rest.service.DiseaseService;
 import uk.ac.ebi.uniprot.ds.rest.service.DrugService;
 import uk.ac.ebi.uniprot.ds.rest.service.ProteinService;
+import uk.ac.ebi.uniprot.ds.rest.service.UniProtSiteMapService;
 import uk.ac.ebi.uniprot.ds.rest.service.VariantService;
 import uk.ac.ebi.uniprot.ds.rest.utils.ModelCreationUtils;
 
@@ -65,6 +66,9 @@ public class ProteinControllerGetDiseasesTest {
 
     @MockBean
     private DrugService drugService;
+
+    @MockBean
+    private UniProtSiteMapService uniProtSiteMapService;
 
     @Test
     public void testNonExistentAccessions() throws Exception {
