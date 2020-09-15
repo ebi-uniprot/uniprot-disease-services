e0e9942334ca066b5e33e20b93b1bc307436f573
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-09-15 10:58:35 +0100

    fix merge conflict

--- disease-rest-service/src/test/java/uk/ac/ebi/uniprot/ds/rest/controller/UniProtSiteMapControllerTest.java
+++ disease-rest-service/src/test/java/uk/ac/ebi/uniprot/ds/rest/controller/UniProtSiteMapControllerTest.java
@@ -125,7 +125,7 @@ public class UniProtSiteMapControllerTest {
         ResultActions res = this.mockMvc.perform
                 (
                         MockMvcRequestBuilders.
-                                get("/v1/ds/sitemaps/" + accession).
+                                get("/v1/ds/ortholog-mappings/" + accession).
                                 param("accession", accession)
                 );
 
@@ -147,7 +147,7 @@ public class UniProtSiteMapControllerTest {
         ResultActions res = this.mockMvc.perform
                 (
                         MockMvcRequestBuilders.
-                                get("/v1/ds/sitemaps/" + accession).
+                                get("/v1/ds/ortholog-mappings/" + accession).
                                 param("accession", accession)
                 );
 
